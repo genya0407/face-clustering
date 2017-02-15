19eeedc771a844314a7b10a1aa6df26aa0625153
Yusuke Sangenya <longinus.eva@gmail.com>
2017-02-15 21:51:44 +0900

    update

--- categorize_faces/lambda_function.py
+++ categorize_faces/lambda_function.py
@@ -1,37 +1,63 @@
 import boto3
-import networkx
+import community
+import collections
+import networkx as nx
+import json
 
 dynamo = boto3.client('dynamodb')
 rekog = boto3.client('rekognition')
 
 def lambda_handler(event, context):
+    try:
+        movie_id = json.loads(event['body'])['movie_id']
+    except Exception as e:
+        print e
+        return
+
     res = dynamo.scan(
-        Limit=1500,
-        TableName="faces"
+        Limit=3000,
+        TableName="faces",
+        ExpressionAttributeValues={
+            ':movie_id': {
+                'S': movie_id,
+            },
+        },
+        FilterExpression='movie_id = :movie_id',
     )
-    print res['Items'][0]
-    """
-            res = rekog.search_faces(
-                CollectionId="cast_faces",
-                FaceId=target_face["face_id"]["S"],
-                FaceMatchThreshold=90
-            )
-        for face_match in res['FaceMatches']:
-            dynamo.update_item(
-                Key={ 
-                    "face_id": {
-                        "S": face_match["Face"]["FaceId"]
-                    },
-                },
-                ExpressionAttributeNames={
-                    '#T': 'tag',
-                },
-                ExpressionAttributeValues={
-                    ':t': {
-                        'N': 'tag'
-                    }
-                },
-                TableName='faces',
-                UpdateExpression="SET #T = :t"
-            )
-    """
+    
+    G = nx.Graph()
+    
+    for item in res["Items"]:
+        G.add_node(item['face_id']["S"])
+        res = rekog.search_faces(
+            CollectionId=movie_id,
+            FaceId=item["face_id"]["S"],
+            FaceMatchThreshold=80,
+            MaxFaces=500
+        )
+        G.add_nodes_from(map(lambda x: x['Face']['FaceId'], res['FaceMatches']))
+        for pair in res['FaceMatches']:
+            G.add_edge(pair['Face']['FaceId'], item['face_id']['S'])
+    
+    partition = community.best_partition(G)
+    
+    for face_id, tag in partition.items():
+        dynamo.update_item(
+            Key={
+                "face_id": {
+                    "S": face_id
+                }
+            },
+            ExpressionAttributeNames={
+                '#T': 'tag',
+            },
+            ExpressionAttributeValues={
+                ':t': {
+                    'S': str(tag)
+                }
+            },
+            TableName='faces',
+            UpdateExpression="SET #T = :t"
+        )
+
+    return
