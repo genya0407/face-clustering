2ee2bf4c7292ef7e93b1a4ac578c09ec9a3d1427
Yusuke Sangenya <longinus.eva@gmail.com>
2017-02-16 06:18:31 +0900

    rewrite with for statement

--- face-tags/lambda_function.py
+++ face-tags/lambda_function.py
@@ -5,7 +5,7 @@ dynamo = boto3.client('dynamodb')
 
 def lambda_handler(event, context):
     movie_id = event['movie_id']
-    return dynamo.scan(
+    res = dynamo.scan(
         ExpressionAttributeValues={
             ':movie_id': {
                 'S': movie_id,
@@ -14,3 +14,19 @@ def lambda_handler(event, context):
         FilterExpression='movie_id = :movie_id',
         TableName='faces'
     )
+
+    max_time = max(map(lambda item: int(item['time']['N']), res['Items']))
+
+    time_to_faces = {}
+    for t in range(1, max_time+1):
+        index = str(t)
+        time_to_faces[index] = []
+        for item in res['Items']:
+            if item['time']['N'] == index:
+                time_to_faces[index].append(int(item['tag']['S']))
+
+    return {
+        'statusCode': 200,
+        'headers': {},
+        'body': json.dumps(time_to_faces)
+    }
