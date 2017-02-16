7caa24884f0b9975fbe381c56d5920302a3c2d91
Yusuke Sangenya <longinus.eva@gmail.com>
2017-02-16 20:49:25 +0900

    list-movies

---
+++ list-movies/lambda_function.py
@@ -0,0 +1,30 @@
+# coding: utf-8
+
+import boto3
+import json
+
+dynamo = boto3.client('dynamodb')
+
+def as_json(response):
+    retval = []
+    for item in response['Items']:
+        retval.append(
+            {
+                'movie_id': item['movie_id']['S'],
+                'object_key': item['object_key']['S'],
+                'capture_finish_at': item.get('capture_finish_at', { 'S': None })['S'], # item['capture_finish_at']が存在しない時にNoneになるように
+                'categorize_finish_at': item.get('categorize_finish_at', { 'S': None })['S']
+            }
+        )
+    return retval
+
+def lambda_handler(event, context):
+    res = dynamo.scan(
+        TableName='movies'
+    )
+
+    return {
+        'body': json.dumps(as_json(res)),
+        'statusCode': 200,
+        'headers': {}
+    }
